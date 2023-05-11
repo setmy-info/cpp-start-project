92e3e2d830dea0c62d0c0bbcd74592770ff50a13
Imre Tabur <imre.tabur@mail.ee>
2023-05-11 17:51:13 +0300

    CMake preparations

--- src/main/cpp/program/main.cpp
+++ src/main/cpp/program/main.cpp
@@ -1,7 +1,18 @@
-#include<iostream>
+#include "main.h"
+#include <cstdlib>
+#include <iostream>
 
+using namespace SetmyInfo;
 using namespace std;
 
-int main() {
+extern char** environ;
+
+int main(const int argc, const char * const argv[]) {
+	char** env = environ;
     cout << "Hello World!" << endl;
+    while (*env != nullptr) {
+           cout << *env << endl;
+           env++;
+    }
+    return 0;
 }
---
+++ src/main/include/program/main.h
@@ -0,0 +1,8 @@
+#ifndef SETMYINFO_MAIN_H
+#define SETMYINFO_MAIN_H
+
+namespace SetmyInfo {
+
+}
+
+#endif // SETMYINFO_MAIN_H
