89887fa3157995feb9a019d545dbe1b559fb97c8
Imre Tabur <imre.tabur@mail.ee>
2023-06-06 22:06:37 +0300

    Test enhancements

--- src/main/cpp/library/CommandLineParameters.cpp
+++ src/main/cpp/library/CommandLineParameters.cpp
@@ -7,17 +7,12 @@
 
 namespace SetMyInfo {
 
-    using CommandLineParametersMap = std::unordered_map<std::string, std::vector<std::string>>;
-
     void CommandLineParameters::Args(const int argc, const char *argv[]) {
         this->commandName = argv[0];
         for (int i = 1; i < argc; i++) {
             std::string tmp = argv[i];
             this->parameters.push_back(tmp);
         }
-
-        CommandLineParametersMap parameters;
-        std::vector<CommandLineParametersMap> parametersList;
 
         for (const std::string parameter: this->parameters) {
             std::istringstream iss(parameter);
@@ -26,10 +21,10 @@ namespace SetMyInfo {
 
             std::string value;
             while (std::getline(iss, value, '=')) {
-                parameters[option].push_back(value);
+                parameters_map[option].push_back(value);
             }
 
-            parameters[option].empty();
+            parameters_map[option];
         }
     }
 }
--- src/test/cpp/library/include/CommandLineParametersTest.h
+++ src/test/cpp/library/include/CommandLineParametersTest.h
@@ -12,10 +12,10 @@ BOOST_AUTO_TEST_SUITE(commandline_parameters_test)
     BOOST_AUTO_TEST_SUITE(ut)
 
         BOOST_AUTO_TEST_CASE(arguments_test) {
-            //CommandLineParameters commandline_parameters;
+            CommandLineParameters commandline_parameters;
             const char *arguments[] = {"programName", "abc", "def=ghi", "jkl=mno=prs"};
             BOOST_CHECK_EQUAL(ARRAY_SIZEOF(arguments), 4);
-            //commandline_parameters.Args(ARRAY_SIZEOF(arguments), arguments);
+            commandline_parameters.Args(ARRAY_SIZEOF(arguments), arguments);
             BOOST_CHECK_EQUAL(1, 1);
         }
 
